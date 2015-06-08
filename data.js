5b18516eb94b8056ea211e508cea1a8d357dd3a8
datLucius <jlnelsonart@gmail.com>
2015-06-08 00:34:25 -0400

    changed theme to kubrick film colors, the arrays are now linked properly, lightbox displays image, buttons working

--- data.js
+++ data.js
@@ -1,214 +1,227 @@
 
 var picAlbums = [
 {
-  name: tacoBell,
-  title: "Taco Bell",
-  image: "TacoBellBuilding.jpg",
+  name: "clockwork",
+  title: "Clockwork Orange",
+  image: "clockWork/clOrCover.png",
 },
 {
-  name: kfc,
-  title: "KFC",
-  image: "kfcBuilding.jpg",
+  name: "spaceOd",
+  title: "2001: A Space Odyssey",
+  image: "spaceOd/spcOdCover.png",
 },
 {
-  name: littleCaesars,
-  title: "Little Caesars",
-  image: "littleCaesarsBuilding.jpg",
+  name: "shining",
+  title: "The Shining",
+  image: "shining/shCover.png",
 },
 {
-  name: butcherBee,
-  title: "Butcher & Bee",
-  image: "butcherBeeBuilding.jpg",
+  name: "fullMetal",
+  title: "Full Metal Jacket",
+  image: "FullMetal/fullMetCover.png",
 },
 {
-  name: cookOut,
-  title: "Cook Out",
-  image: "cookOutBuilding.jpg",
+  name: "barryLyndon",
+  title: "Barry Lyndon",
+  image: "barryLyndon/bLCover.png",
 },
 {
-  name: waffleHouse,
-  title: "Waffle House",
-  image: "waffleHouseBuilding.jpg",
+  name: "eyesWS",
+  title: "Eyes Wide Shut",
+  image: "eyesWS/eyesWSCover.png",
 }
 ];
 
-var tacoBell = [
+var spaceOdArray = [
 {
-  title: "Dorrito Taco",
-  image: "dorritotaco.jpg",
+  title: "13:07",
+  image: "spaceOd/SpOcPl4.png",
 },
 {
-  title: "Crunchwrap Supreme",
-  image: "Crunchwrap-Supreme.jpg",
+  title: "30:57",
+  image: "spaceOd/SpOcPl5.png",
 },
 {
-  title: "Breakfast Crunchwrap",
-  image: "firstmeal.gif",
+  title: "1:07:58",
+  image: "spaceOd/SpOcPl3.png",
 },
 {
-  title: "Gordita",
-  image: "gordita.jpg",
+  title: "1:26:18",
+  image: "spaceOd/SpOcPl2.png",
 },
 {
-  title: "Volcano Nacho",
-  image: "volcanonacho.jpg",
+  title: "2:09:43",
+  image: "spaceOd/SpOcPl6.png",
 },
 {
-  title: "Cheesy Beef Burrito",
-  image: "cheesyBeef.jpg",
+  title: "2:16:44",
+  image: "spaceOd/SpOcPl1.png",
+}
+];
+var clockworkArray = [
+{
+  title: "0:10",
+  image: "clockWork/clOr1.png",
+},
+{
+  title: "25:14",
+  image: "clockWork/clOr2.png",
+},
+{
+  title: "33:24",
+  image: "clockWork/clOr3.png",
+},
+{
+  title: "1:04:29",
+  image: "clockWork/clOr4.png",
+},
+{
+  title: "1:29:22",
+  image: "clockWork/clOr5.png",
+},
+{
+  title: "1:43:40",
+  image: "clockWork/clOr6.png",
+}
+];
+var shiningArray = [
+{
+  title: "1:17",
+  image: "shining/sh1.png",
+},
+{
+  title: "15:23",
+  image: "shining/sh2.png",
+},
+{
+  title: "45:14",
+  image: "shining/sh3.png",
+},
+{
+  title: "1:24:25",
+  image: "shining/sh4.png",
+},
+{
+  title: "1:33:12",
+  image: "shining/sh5.png",
+},
+{
+  title: "1:53:15",
+  image: "shining/sh6.png",
+}
+];
+var fullMetArray = [
+{
+  title: "3:19",
+  image: "FullMetal/fullMet1.png",
+},
+{
+  title: "40:14",
+  image: "FullMetal/fullMet2.png",
+},
+{
+  title: "50:56",
+  image: "FullMetal/fullMet3.png",
+},
+{
+  title: "1:01:01",
+  image: "FullMetal/fullMet4.png",
+},
+{
+  title: "1:20:42",
+  image: "FullMetal/fullMet5.png",
+},
+{
+  title: "2:23:40",
+  image: "FullMetal/fullMet6.png",
+}
+];
+var barryLyndonArray = [
+{
+  title: "1:18",
+  image: "barryLyndon/bL1.png",
+},
+{
+  title: "27:27",
+  image: "barryLyndon/bL2.png",
+},
+{
+  title: "36:50",
+  image: "barryLyndon/bL3.png",
+},
+{
+  title: "1:04:29",
+  image: "barryLyndon/bL4.png",
+},
+{
+  title: "1:49:22",
+  image: "barryLyndon/bL5.png",
+},
+{
+  title: "2:43:45",
+  image: "barryLyndon/bL6.png",
 }
 ];
 
-var kfc = [
+var eyesWSArray = [
 {
-  title: "Bucket-o-Chicken",
-  image: "bucketChicken.gif",
+  title: "2:38",
+  image: "eyesWS/eyesWS1.png",
 },
 {
-  title: "Crunchwrap Supreme",
-  image: "Crunchwrap-Supreme.jpg",
+  title: "25:14",
+  image: "eyesWS/eyesWS2.png",
 },
 {
-  title: "Breakfast Crunchwrap",
-  image: "firstmeal.gif",
+  title: "36:35",
+  image: "eyesWS/eyesWS3.png",
 },
 {
-  title: "Gordita",
-  image: "gordita.jpg",
+  title: "1:04:29",
+  image: "eyesWS/eyesWS4.png",
 },
 {
-  title: "Volcano Nacho",
-  image: "volcanonacho.jpg",
+  title: "1:29:22",
+  image: "eyesWS/eyesWS5.png",
 },
 {
-  title: "Cheesy Beef Burrito",
-  image: "cheesyBeef.jpg",
-}
-];
-var littleCaesars = [
-{
-  title: "Bucket-o-Chicken",
-  image: "bucketChicken.gif",
-},
-{
-  title: "Crunchwrap Supreme",
-  image: "Crunchwrap-Supreme.jpg",
-},
-{
-  title: "Breakfast Crunchwrap",
-  image: "firstmeal.gif",
-},
-{
-  title: "Gordita",
-  image: "gordita.jpg",
-},
-{
-  title: "Volcano Nacho",
-  image: "volcanonacho.jpg",
-},
-{
-  title: "Cheesy Beef Burrito",
-  image: "cheesyBeef.jpg",
-}
-];
-var butcherBee = [
-{
-  title: "Bucket-o-Chicken",
-  image: "bucketChicken.gif",
-},
-{
-  title: "Crunchwrap Supreme",
-  image: "Crunchwrap-Supreme.jpg",
-},
-{
-  title: "Breakfast Crunchwrap",
-  image: "firstmeal.gif",
-},
-{
-  title: "Gordita",
-  image: "gordita.jpg",
-},
-{
-  title: "Volcano Nacho",
-  image: "volcanonacho.jpg",
-},
-{
-  title: "Cheesy Beef Burrito",
-  image: "cheesyBeef.jpg",
-}
-];
-var cookOut = [
-{
-  title: "Bucket-o-Chicken",
-  image: "bucketChicken.gif",
-},
-{
-  title: "Crunchwrap Supreme",
-  image: "Crunchwrap-Supreme.jpg",
-},
-{
-  title: "Breakfast Crunchwrap",
-  image: "firstmeal.gif",
-},
-{
-  title: "Gordita",
-  image: "gordita.jpg",
-},
-{
-  title: "Volcano Nacho",
-  image: "volcanonacho.jpg",
-},
-{
-  title: "Cheesy Beef Burrito",
-  image: "cheesyBeef.jpg",
+  title: "1:43:40",
+  image: "eyesWS/eyesWS6.png",
 }
 ];
 
-var waffleHouse = [
-{
-  title: "Bucket-o-Chicken",
-  image: "bucketChicken.gif",
-},
-{
-  title: "Crunchwrap Supreme",
-  image: "Crunchwrap-Supreme.jpg",
-},
-{
-  title: "Breakfast Crunchwrap",
-  image: "firstmeal.gif",
-},
-{
-  title: "Gordita",
-  image: "gordita.jpg",
-},
-{
-  title: "Volcano Nacho",
-  image: "volcanonacho.jpg",
-},
-{
-  title: "Cheesy Beef Burrito",
-  image: "cheesyBeef.jpg",
-}
-];
 
-var templates = {};
+var selectedAlbum;
+var albumsPics = {
+  spaceOd: spaceOdArray,
+  clockwork: clockworkArray,
+  shining: shiningArray,
+  fullMetal: fullMetArray,
+  barryLyndon: barryLyndonArray,
+  eyesWS: eyesWSArray
+};
 
-var selectedAlbum = $(this).attr('name');
+
+$('.albumGallery').on('click', '.imgDiv', function(event) {
+    event.preventDefault();
+   selectedAlbum = albumsPics[$(this).attr('data-name')];
+   var imgString = "";
+   selectedAlbum.forEach(function(el) {
+     imgString += compiledTmpl(el);
+   });
+   $('.imageGallery').append(imgString);
+ });
 
 var mytmpl = [
- "<div class='col-md-4 col-sm-6 imgDiv'>",
- "<img class='imageCover' src='<%= image %>'>",
+ "<div class='col-md-4 col-sm-6 imgDiv' data-name='<%= name %>' >",
+ "<img class='imageCover' src='<%= image %>'> ",
  "<h3 class='caption'><%= title %></h3>",
  "</div>"
 ].join("");
 
 
 var compiledTmpl = _.template(mytmpl);
-var imgString = "";
 
-tacoBell.forEach(function(el) {
-  imgString += compiledTmpl(el);
-});
 
 var albString = "";
 
@@ -217,28 +230,3 @@ picAlbums.forEach(function(el) {
 });
 
 $('.albumGallery').append(albString);
-
-$('.imageGallery').append(imgString);
-
-var overlay = $('<div class="overlay"></div>');
-var imageBig = $("<img>");
-var caption = $("<p></p>");
-
-overlay.append(imageBig);
-
-overlay.append(caption);
-
-//OVERLAY//
-$("body").append(overlay);
-
-$(".imageGallery .imgCover").click(function(event){
-  event.preventDefault();
-  var imageLocation = $(this).attr("image");
-
-  image.attr("src", imageLocation);
-
-  overlay.show();
-
-  var captionText = $(this).attr("title");
-  caption.text(captionText);
-});
--- main.js
+++ main.js
@@ -3,22 +3,63 @@ $(document).ready(function() {
   $('.sideBar').hide();
   $('.imageGalleryContent').hide();
 
-
   $('.albumGallery').on('click', '.imgDiv', function(event) {
       event.preventDefault();
 
-      $('header').slideUp(800);
+    $('header').slideUp(800);
 
      $('.albumGallery').hide();
 
      $('.sideBar').show();
 
+     var albumName = $(this).attr('title');
+
+     var selectedAlbum = $(this).attr('name');
 
      $('.imageGalleryContent').show();
+
+     $('.imageGalleryHeaderText').html(albumName);
+
+
+  });
+
+  $('.sideBar').on('click', '.albumButton', function(event) {
+      event.preventDefault();
 
      var albumName = $(this).attr('title');
 
      $('.imageGalleryHeaderText').html(albumName);
 
+     selectedAlbum = albumsPics[$(this).attr('name')];
+     var imgString = "";
+     selectedAlbum.forEach(function(el) {
+       imgString += compiledTmpl(el);
+     });
+     $('.imageGallery').html(imgString);
   });
+
+  var overlay = $('<div class="overlay hide"></div>');
+  var imageBig = $("<img>");
+  var caption = $("<p></p>");
+
+
+
+  //OVERLAY//
+  $("body").append(overlay);
+
+  $('.imageGallery').on('click', '.imageCover', function(event){
+    event.preventDefault();
+    var imageLocation = $(this).attr('src');
+    var captionText = $(this).attr('data-name');
+
+    imageBig.attr("src", imageLocation);
+
+    overlay.append(imageBig);
+
+    overlay.append(caption);
+
+    overlay.removeClass('hide');
+
+  });
+
 });
